50147eb90dad3ff61197c6d3b895f1ccc7b309a3
ASDomoroschenov <sashachicha228@mail.ru>
2024-04-26 14:38:26 +0300

    rename project

---
+++ ChatService/src/main/java/ru/mai/crypto/TestApplication.java
@@ -0,0 +1,13 @@
+package ru.mai.crypto;
+
+import com.vaadin.flow.component.page.Push;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
