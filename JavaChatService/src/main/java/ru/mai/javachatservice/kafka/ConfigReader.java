42ebc0da9b7f5f27371f37088878eb2d134b5c99
ASDomoroschenov <sashachicha228@mail.ru>
2024-05-02 21:56:22 +0300

    prepare to integrate postgresql

--- JavaChatService/src/main/java/ru/mai/javachatservice/kafka/ConfigReader.java
+++
@@ -1,7 +0,0 @@
-package ru.mai.javachatservice.kafka;
-
-import com.typesafe.config.Config;
-
-public interface ConfigReader {
-    Config loadConfig();
-}
--- JavaChatService/src/main/java/ru/mai/javachatservice/kafka/KafkaReader.java
+++
@@ -1,6 +0,0 @@
-package ru.mai.javachatservice.kafka;
-
-public interface KafkaReader {
-    public void processing(String inputTopic);
-    public void close();
-}
