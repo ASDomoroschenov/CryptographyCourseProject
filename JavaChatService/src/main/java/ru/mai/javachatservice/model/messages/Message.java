5c1f9377a12514a935f12025f40399be006c7da8
ASDomoroschenov <sashachicha228@mail.ru>
2024-05-04 23:48:35 +0300

    commit before kafka+spring

--- JavaChatService/src/main/java/ru/mai/javachatservice/model/messages/Message.java
+++ JavaChatService/src/main/java/ru/mai/javachatservice/model/messages/Message.java
@@ -30,4 +30,15 @@ public class Message {
 
         return new byte[0];
     }
+
+    @Override
+    public String toString() {
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException ex) {
+            log.error("Error while processing message to json bytes");
+        }
+
+        return null;
+    }
 }
